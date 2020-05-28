b556b629b4a8540808307d69bee26303f3ab1eed
Jhon Giraldo <jhonferley18@gmail.com>
2020-05-28 16:22:31 -0500

    Cambios en conexion

---
+++ app/Config/DatosConexion.php
@@ -0,0 +1,56 @@
+<?php  
+
+	/*
+		Archivo encargado de la configuracion de datos
+		para la base de datos y rutas estaticas
+		tambien del controlador por defecto y metodo por defecto
+	*/
+
+	
+	//datos conexion sqlserver
+    define("DBNAME","HOSVITAL");
+	define("DBUSER","sa");
+	define("DBPASS","rRDi4dKj4w1Vwl3peXL8");
+	define("DBHOST","10.10.10.115");
+	define("DBCHARSET","utf8");
+
+	/*INICIO SERVIDOR LOCAL */
+
+	//datos conexion mysql
+	define("DBNAMEM","bdestadistica");
+	define("DBUSERM","root");
+	define("DBPASSM","");
+	define("DBHOSTM","localhost");
+	
+	define("BASEURL","http://localhost/UCI/EstadisticaEnfermeria/");//para llamados a archivos
+	define("BASEPATH","C:\\xampp\\htdocs\\UCI\\EstadisticaEnfermeria\\");//para imagenes
+
+	/*FIN CONEXION LOCAL*/
+
+
+
+	/*INICIO CONEXION SERVIDOR UBUNTU*/
+	/*
+	//datos conexion mysql
+	define("DBNAMEM",'bdestadistica');
+	define("DBUSERM",'phpmyadmin');
+	define("DBPASSM",'1234');
+	define("DBHOSTM",'localhost');
+	
+	define("BASEURL","http://10.10.10.110:8081/serviucis/desarrollo/EstadisticaEnfermeria/");//para llamados a archivos
+	define("BASEPATH","/var/www/html/serviucis/desarrollo/EstadisticaEnfermeria/");//para imagenes
+	*/
+	/*FIN CONEXION SERVIDOR UBUNTU*/
+
+
+	//muestra el controlador si no envia un controlador(controllers) por GET
+	define("DefaultController","ControllerLogin");
+	// muestra el metodo si no envia una accion(metodo) por GET
+	define("ActionDefault", "Iniciar");
+	
+	define("VERSION","1.1 PROD");
+
+	//indicamos la configuracion de la zona
+    date_default_timezone_set("America/Bogota");
+    
+?>    
