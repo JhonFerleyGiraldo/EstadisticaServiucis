a548ff3d0ae6a9f7037b86665a5fee950db30446
SERVIDOR UBUNTU PRODUCCION <jhonferley18@gmail.com>
2020-06-01 08:28:12 -0500

    Modificacion conexion servidor ubuntu pruebas

--- app/Config/DatosConexion.php
+++ app/Config/DatosConexion.php
@@ -15,7 +15,7 @@
 	define("DBCHARSET","utf8");
 
 	/*INICIO SERVIDOR LOCAL */
-
+/*
 	//datos conexion mysql
 	define("DBNAMEM","bdestadistica");
 	define("DBUSERM","root");
@@ -24,22 +24,22 @@
 	
 	define("BASEURL","http://localhost/UCI/EstadisticaEnfermeria/");//para llamados a archivos
 	define("BASEPATH","C:\\xampp\\htdocs\\UCI\\EstadisticaEnfermeria\\");//para imagenes
-
+*/
 	/*FIN CONEXION LOCAL*/
 
 
 
 	/*INICIO CONEXION SERVIDOR UBUNTU*/
-	/*
+	
 	//datos conexion mysql
-	define("DBNAMEM",'bdestadistica');
+	define("DBNAMEM",'bdestadisticaPRUEBAS');
 	define("DBUSERM",'phpmyadmin');
 	define("DBPASSM",'1234');
 	define("DBHOSTM",'localhost');
 	
-	define("BASEURL","http://10.10.10.110:8081/serviucis/desarrollo/EstadisticaEnfermeria/");//para llamados a archivos
-	define("BASEPATH","/var/www/html/serviucis/desarrollo/EstadisticaEnfermeria/");//para imagenes
-	*/
+	define("BASEURL","http://10.10.10.110:8081/serviucis/pruebas/EstadisticaServiucis/");//para llamados a archivos
+	define("BASEPATH","/var/www/html/serviucis/pruebas/EstadisticaServiucis/");//para imagenes
+	
 	/*FIN CONEXION SERVIDOR UBUNTU*/
 
 
@@ -48,7 +48,7 @@
 	// muestra el metodo si no envia una accion(metodo) por GET
 	define("ActionDefault", "Iniciar");
 	
-	define("VERSION","1.1 PROD");
+	define("VERSION","1.1 PRUE");
 
 	//indicamos la configuracion de la zona
     date_default_timezone_set("America/Bogota");
